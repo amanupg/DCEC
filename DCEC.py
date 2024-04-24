1ee53f8c68e5203d88269a2cb2db8a4bee6afa15
amanupg <105249745+amanupg@users.noreply.github.com>
2024-04-24 17:54:00 +0530

    Update DCEC.py

--- DCEC.py
+++ DCEC.py
@@ -7,7 +7,7 @@ from keras.utils.vis_utils import plot_model
 from sklearn.cluster import KMeans
 import metrics
 from ConvAE import CAE
-
+from datasets import load_custom_dataset  # Import the custom dataset loader function
 
 class ClusteringLayer(Layer):
     """
@@ -148,118 +148,4 @@ class DCEC(object):
         t1 = time()
         print('Initializing cluster centers with k-means.')
         kmeans = KMeans(n_clusters=self.n_clusters, n_init=20)
-        self.y_pred = kmeans.fit_predict(self.encoder.predict(x))
-        y_pred_last = np.copy(self.y_pred)
-        self.model.get_layer(name='clustering').set_weights([kmeans.cluster_centers_])
-
-        # Step 3: deep clustering
-        # logging file
-        import csv, os
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        logfile = open(save_dir + '/dcec_log.csv', 'w')
-        logwriter = csv.DictWriter(logfile, fieldnames=['iter', 'acc', 'nmi', 'ari', 'L', 'Lc', 'Lr'])
-        logwriter.writeheader()
-
-        t2 = time()
-        loss = [0, 0, 0]
-        index = 0
-        for ite in range(int(maxiter)):
-            if ite % update_interval == 0:
-                q, _ = self.model.predict(x, verbose=0)
-                p = self.target_distribution(q)  # update the auxiliary target distribution p
-
-                # evaluate the clustering performance
-                self.y_pred = q.argmax(1)
-                if y is not None:
-                    acc = np.round(metrics.acc(y, self.y_pred), 5)
-                    nmi = np.round(metrics.nmi(y, self.y_pred), 5)
-                    ari = np.round(metrics.ari(y, self.y_pred), 5)
-                    loss = np.round(loss, 5)
-                    logdict = dict(iter=ite, acc=acc, nmi=nmi, ari=ari, L=loss[0], Lc=loss[1], Lr=loss[2])
-                    logwriter.writerow(logdict)
-                    print('Iter', ite, ': Acc', acc, ', nmi', nmi, ', ari', ari, '; loss=', loss)
-
-                # check stop criterion
-                delta_label = np.sum(self.y_pred != y_pred_last).astype(np.float32) / self.y_pred.shape[0]
-                y_pred_last = np.copy(self.y_pred)
-                if ite > 0 and delta_label < tol:
-                    print('delta_label ', delta_label, '< tol ', tol)
-                    print('Reached tolerance threshold. Stopping training.')
-                    logfile.close()
-                    break
-
-            # train on batch
-            if (index + 1) * batch_size > x.shape[0]:
-                loss = self.model.train_on_batch(x=x[index * batch_size::],
-                                                 y=[p[index * batch_size::], x[index * batch_size::]])
-                index = 0
-            else:
-                loss = self.model.train_on_batch(x=x[index * batch_size:(index + 1) * batch_size],
-                                                 y=[p[index * batch_size:(index + 1) * batch_size],
-                                                    x[index * batch_size:(index + 1) * batch_size]])
-                index += 1
-
-            # save intermediate model
-            if ite % save_interval == 0:
-                # save DCEC model checkpoints
-                print('saving model to:', save_dir + '/dcec_model_' + str(ite) + '.h5')
-                self.model.save_weights(save_dir + '/dcec_model_' + str(ite) + '.h5')
-
-            ite += 1
-
-        # save the trained model
-        logfile.close()
-        print('saving model to:', save_dir + '/dcec_model_final.h5')
-        self.model.save_weights(save_dir + '/dcec_model_final.h5')
-        t3 = time()
-        print('Pretrain time:  ', t1 - t0)
-        print('Clustering time:', t3 - t1)
-        print('Total time:     ', t3 - t0)
-
-
-if __name__ == "__main__":
-    # setting the hyper parameters
-    import argparse
-    parser = argparse.ArgumentParser(description='train')
-    parser.add_argument('dataset', default='mnist', choices=['mnist', 'usps', 'mnist-test'])
-    parser.add_argument('--n_clusters', default=10, type=int)
-    parser.add_argument('--batch_size', default=256, type=int)
-    parser.add_argument('--maxiter', default=2e4, type=int)
-    parser.add_argument('--gamma', default=0.1, type=float,
-                        help='coefficient of clustering loss')
-    parser.add_argument('--update_interval', default=140, type=int)
-    parser.add_argument('--tol', default=0.001, type=float)
-    parser.add_argument('--cae_weights', default=None, help='This argument must be given')
-    parser.add_argument('--save_dir', default='results/temp')
-    args = parser.parse_args()
-    print(args)
-
-    import os
-    if not os.path.exists(args.save_dir):
-        os.makedirs(args.save_dir)
-
-    # load dataset
-    from datasets import load_mnist, load_usps
-    if args.dataset == 'mnist':
-        x, y = load_mnist()
-    elif args.dataset == 'usps':
-        x, y = load_usps('data/usps')
-    elif args.dataset == 'mnist-test':
-        x, y = load_mnist()
-        x, y = x[60000:], y[60000:]
-
-    # prepare the DCEC model
-    dcec = DCEC(input_shape=x.shape[1:], filters=[32, 64, 128, 10], n_clusters=args.n_clusters)
-    plot_model(dcec.model, to_file=args.save_dir + '/dcec_model.png', show_shapes=True)
-    dcec.model.summary()
-
-    # begin clustering.
-    optimizer = 'adam'
-    dcec.compile(loss=['kld', 'mse'], loss_weights=[args.gamma, 1], optimizer=optimizer)
-    dcec.fit(x, y=y, tol=args.tol, maxiter=args.maxiter,
-             update_interval=args.update_interval,
-             save_dir=args.save_dir,
-             cae_weights=args.cae_weights)
-    y_pred = dcec.y_pred
-    print('acc = %.4f, nmi = %.4f, ari = %.4f' % (metrics.acc(y, y_pred), metrics.nmi(y, y_pred), metrics.ari(y, y_pred)))
+        self.y_pred = k
