7af5347ed4cf75a6e926339ea9e15df61e8ea9e3
amanupg <105249745+amanupg@users.noreply.github.com>
2024-04-24 17:35:45 +0530

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,46 +1,27 @@
 import numpy as np
+import os
+import cv2
 
-
-def load_mnist():
-    # the data, shuffled and split between train and test sets
-    from keras.datasets import mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    x = np.concatenate((x_train, x_test))
-    y = np.concatenate((y_train, y_test))
-    x = x.reshape(-1, 28, 28, 1).astype('float32')
-    x = x/255.
-    print('MNIST:', x.shape)
+def load_custom_dataset(data_path='./data/custom_dataset'):
+    x = []
+    y = []
+    for label in os.listdir(data_path):
+        label_path = os.path.join(data_path, label)
+        if os.path.isdir(label_path):
+            for image_file in os.listdir(label_path):
+                image_path = os.path.join(label_path, image_file)
+                # Read image
+                img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+                # Resize image to 28x28 if necessary
+                img = cv2.resize(img, (28, 28))
+                # Normalize pixel values to [0, 1]
+                img = img.astype('float32') / 255.
+                # Append image and label to lists
+                x.append(img)
+                y.append(int(label))
+    x = np.array(x)
+    y = np.array(y)
+    print('Custom dataset:', x.shape, y.shape)
     return x, y
 
-
-def load_usps(data_path='./data/usps'):
-    import os
-    if not os.path.exists(data_path+'/usps_train.jf'):
-        if not os.path.exists(data_path+'/usps_train.jf.gz'):
-            os.system('wget http://www-i6.informatik.rwth-aachen.de/~keysers/usps_train.jf.gz -P %s' % data_path)
-            os.system('wget http://www-i6.informatik.rwth-aachen.de/~keysers/usps_test.jf.gz -P %s' % data_path)
-        os.system('gunzip %s/usps_train.jf.gz' % data_path)
-        os.system('gunzip %s/usps_test.jf.gz' % data_path)
-
-    with open(data_path + '/usps_train.jf') as f:
-        data = f.readlines()
-    data = data[1:-1]
-    data = [list(map(float, line.split())) for line in data]
-    data = np.array(data)
-    data_train, labels_train = data[:, 1:], data[:, 0]
-
-    with open(data_path + '/usps_test.jf') as f:
-        data = f.readlines()
-    data = data[1:-1]
-    data = [list(map(float, line.split())) for line in data]
-    data = np.array(data)
-    data_test, labels_test = data[:, 1:], data[:, 0]
-
-    x = np.concatenate((data_train, data_test)).astype('float32')
-    x /= 2.0
-    x = x.reshape([-1, 16, 16, 1])
-    y = np.concatenate((labels_train, labels_test))
-    print('USPS samples', x.shape)
-    return x, y
 
